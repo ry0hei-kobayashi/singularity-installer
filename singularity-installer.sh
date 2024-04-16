da9a1ef5d19907e98215494460bb67c339599757
ryohei-kobayashi <ryohei.kobayashi@okadanet.org>
2024-04-17 08:48:05 +0900

    first commit

---
+++ singularity-installer.sh
@@ -0,0 +1,62 @@
+sudo apt update
+
+#install docker
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install docker-ce
+
+#docker sudo 
+
+#install nessesary
+# Ensure repositories are up-to-date
+sudo apt-get update
+# Install debian packages for dependencies
+sudo apt-get install -y \
+   autoconf \
+   automake \
+   cryptsetup \
+   git \
+   libfuse-dev \
+   libglib2.0-dev \
+   libseccomp-dev \
+   libtool \
+   pkg-config \
+   runc \
+   squashfs-tools \
+   squashfs-tools-ng \
+   uidmap \
+   wget \
+   zlib1g-dev
+
+#install go
+# rm -r /usr/local/go # remove prev go
+#最新のGOのtarファイルを持ってきて/usr/local/で解凍
+export VERSION=1.21.0 OS=linux ARCH=amd64 && \
+  wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
+  sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
+  rm go$VERSION.$OS-$ARCH.tar.gz
+
+echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.bashrc && \
+  source ~/.bashrc
+
+# download singularity
+export VERSION=4.1.0 && \
+    wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-ce-${VERSION}.tar.gz && \
+    tar -xzf singularity-ce-${VERSION}.tar.gz && \
+    cd singularity-ce-${VERSION}
+
+
+
+#install singularity
+export VERSION=3.8.0 
+wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-ce-${VERSION}.tar.gz
+tar -xzf singularity-ce-${VERSION}.tar.gz 
+cd singularity-ce-${VERSION}
+
+./mconfig
+make -C ./builddir
+sudo make -C ./builddir install
+
+singularity help
